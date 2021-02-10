6fec7bcc3784bbeff87a91b82c53eb030db51eb3
Bieldeandrade <69852854+Bieldeandrade@users.noreply.github.com>
2021-02-10 11:07:13 -0300

    Login completo

---
+++ estudo/session01.php
@@ -0,0 +1,16 @@
+<?php 
+  
+$valor = 10;
+
+session_start(); 
+
+if (isset($_SESSION[views])) 
+	$_SESSION[views] = $_SESSION[views]+1;
+else
+	$_SESSION[views] = 1;
+echo "views =" $_SESSION[views];
+echo "valor =" $valor;
+
+$views = $_SESSION['views'];  
+  
+ ?> 
